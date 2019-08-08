cd4391354c3e018e36c7bc3d2c09f88df512020c
Tofek Salameh <tofek1994s@gmail.com>
2019-08-08 03:30:56 +0300

    backtracking scheduling

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 from flask import Flask
 import pymongo
+from werkzeug.wrappers import json
 
 app = Flask(__name__)
 
 
 def openDatabase():
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["mydatabase"]
-    mycol = mydb["customers"]
+    mydb = myclient["users"]
+    mycol = mydb["schedule"]
     return mycol
 
 
@@ -44,6 +45,43 @@ def getMember():
     for member in mycol.find():
         print(member)
     return "hello"
+
+
+@app.route("/getScheduling")
+def getScheduling():
+    members_map = {}
+    result = {}
+    result = get_optimal_scheduling(list(mycol.find()), 0, members_map)
+    return str(result)
+
+
+def get_optimal_scheduling(members, index, members_map):
+    if index >= len(members):
+        return members_map
+    max_len = -1
+    max_map = {}
+    for i in range(1, 4):
+        # if member can be scheduled in this day
+        if i.__str__() in members[index]['days'].split(','):
+            print(members[index]['name'])
+            print("is in day number:")
+            print(i)
+            members_map[members[index]['name']] = i
+
+            result1 = get_optimal_scheduling(members, index + 1, members_map)
+            print(str(result1))
+            if map_values_length(result1) > max_len:
+                max_len = map_values_length(result1)
+                max_map = result1.copy()
+            del members_map[members[index]['name']]
+    return max_map
+
+def map_values_length(members_map):
+    items = members_map.items()
+    list_of_values = list()
+    for item in items:
+        list_of_values.append(item[1])
+    return len(set(list_of_values))
 
 
 if __name__ == "__main__":
